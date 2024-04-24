3fff21baa11798e9a0e090676bcabe4fd57eb48e
Ramesh S <98960684+Ram509@users.noreply.github.com>
2024-04-24 14:21:18 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import About from "./components/Header.jsx
-import Contact from "./components/Contact.jsx
-import Service from "./components/Service.jsx
+
 
 import App from "./App.js";
 
