d5dd0d47e3c32dbd7b01d589107062e11be2e393
Ramesh S <98960684+Ram509@users.noreply.github.com>
2024-04-24 14:21:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-
+import About from "./components/Header.jsx
+import Contact from "./components/Contact.jsx
+import Service from "./components/Service.jsx
 const App = () => {
   return <div>App</div>;
 };
